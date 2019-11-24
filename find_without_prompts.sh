87070ba9e4f85158c01573c3eb09677e6135aba5
vinanrra <vicentefernandezguerra@gmail.com>
2019-11-24 21:49:04 +0100

    Update find_without_prompts.sh

--- find_without_prompts.sh
+++ find_without_prompts.sh
@@ -1,4 +1,4 @@
 #Usage ./find_without_prompts.sh /path/to/folder string extension
 ##Example ./find_without_prompts.sh /home/vinanrra/test HelloWorld html
-echo Buscando archivos en la carpeta $0 que contengan $1 y tengan extension $2
-find $0 -name "*.$2" -print | grep -i $1
+echo Buscando archivos en la carpeta $0 con extension $1 y contengan $2
+find $0 -name "*.$1" -print | grep -i $2
