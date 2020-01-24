1f92f9c234e555acf93ace96e5ce530c688026b0
vinanrra <vicentefernandezguerra@gmail.com>
2020-01-24 09:24:33 +0100

    Update freenom_update.sh

--- Bash/freenom_update.sh
+++ Bash/freenom_update.sh
@@ -1,35 +1,63 @@
 #!/bin/bash
 
-# Requeriments:
-# https://github.com/dabendan2/freenom-dns
+# Github
+# https://github.com/vinanrra/Scripts-Linux
 
-myip="$(cat ip.txt)"
-newip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+### LOG
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>./freenom-updater/logs/freenom.log 2>&1
+# Everything below will go to the file 'freenom-log.log':
+### LOG
+
+# Store date to variable
 now=$(date)
+
+# Get log size
+file_size=`du -b ./freenom-updater/freenom.log | tr -s '\t' ' ' | cut -d' ' -f1`
+
+# Check log size and if its bigget than 2MB move to freenom-logs
+# If folder doesnt exist it will be created.
 MaxFileSize=2048
-
-file_size=`du -b ./freenom.log | tr -s '\t' ' ' | cut -d' ' -f1`
-
 if [ $file_size -gt $MaxFileSize ];then
+
     timestamp=`date +%s`
-    mkdir -p ./freenom-logs
-    mv ./freenom.log ./freenom-logs/freenom.log.$(date +%m_%d_%H_%Y)
+    mkdir -p ./freenom-updater/logs
+    mv ./freenom.log ./freenom-updater/logs/freenom.log.$(date +%m_%d_%H_%Y)
     touch ./freenom.log
 fi
 
-dig +short myip.opendns.com @resolver1.opendns.com > ./ip.txt
+# Get IP and store to file
+dig +short myip.opendns.com @resolver1.opendns.com > ./freenom-updater/ip.txt
+
+# Load IP from ip.txt to a variable
+myip="$(cat ./freenom-updater/ip.txt)"
+
+# Get actual IP from myip.opendns.com
+newip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
+# Print info with OLD and NEW IP that will be store to log
 
 echo ----------------------------------------------------------
 echo "Checking if the have changed IP..."
 echo "NEW = $newip"
 echo "OLD = $myip"
 
+# Check IPs if they are equal stop script
+# if they are different will update DNS
 if [ "$myip" == "$newip" ];then
+
     echo IPs are equal, the update isnt need it.
     echo $now
     echo ----------------------------------------------------------
-else
-   freenom-dns set www.vfguerra.tk A $myip
+    
+    else
+
+   # Execute freenom-dns to update DNS with new IP
+   # You can change commands to others if you dont use freenom-dns
+   # https://github.com/dabendan2/freenom-dns
+
+   freenom-dns set www.DOMAIN.tk A $myip
    sleep 1m
    freenom-dns set .DOMAIN.tk A $myip
    sleep 1m
@@ -46,4 +74,5 @@ else
    freenom-dns set SUBDOMAIN.DOMAIN.tk A $myip
    echo $now
    echo ----------------------------------------------------------
+   
 fi
