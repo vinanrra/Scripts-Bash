b01fcf133d5a11dba061bc3fcbd42d5dd284fb10
vinanrra <vicentefernandezguerra@gmail.com>
2020-01-24 09:24:58 +0100

    Update freenom_update.sh

--- Bash/freenom_update.sh
+++ Bash/freenom_update.sh
@@ -4,9 +4,11 @@
 # https://github.com/vinanrra/Scripts-Linux
 
 ### LOG
+
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>./freenom-updater/logs/freenom.log 2>&1
+
 # Everything below will go to the file 'freenom-log.log':
 ### LOG
 
